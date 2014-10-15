26f07e85cdd2fc6956703430a9dda371e71b77c7
Ivoah <ivoahivoah@gmail.com>
2014-10-15 10:53:11 -0400

    Fixed indentation

--- demos/ControlLED/ControlLED.ino
+++ demos/ControlLED/ControlLED.ino
@@ -6,12 +6,12 @@ int lineWhite = 3;
 TILP tilp = TILP(lineRed, lineWhite)
 
 void setup() {
-  tilp.begin();
+	tilp.begin();
 }
 
 void loop() {
-  uint8_t header[4] = {COMP83P, KEY, 0xA6, 0x00};
-  int rlen = 0;
-  tilp.send(header, NULL, 0);
-  tilp.get(header, NULL, &rlen, 0);
+	uint8_t header[4] = {COMP83P, KEY, 0xA6, 0x00};
+	int rlen = 0;
+	tilp.send(header, NULL, 0);
+	tilp.get(header, NULL, &rlen, 0);
 }
